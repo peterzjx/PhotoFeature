ed3118958a58fae5a4e8f88d9ad2b7e99b92494f
peterzjx <peterzjx@gmail.com>
2015-03-07 23:19:32 -0500

    simple features

--- py/main.py
+++ py/main.py
@@ -1,7 +1,7 @@
 __author__ = 'Peter'
 from PIL import ImageStat
 from PIL import Image
-from PIL import ImageMath
+from PIL import ImageMath, ImageEnhance
 import math
 
 
@@ -29,7 +29,7 @@ def colorfulness(im):
     im_yb = ImageMath.eval("convert((a + b)/2 - c, 'L')", a=r, b=g, c=b)
     stat_rg = ImageStat.Stat(im_rg)
     stat_yb = ImageStat.Stat(im_yb)
-    sigma_rg, sigma_yb = stat_rg.var[0], stat_yb.var[0]
+    sigma_rg, sigma_yb = stat_rg.stddev[0], stat_yb.stddev[0]
     mu_rg, mu_yb = stat_rg.mean[0], stat_yb.mean[0]
     sigma_rgyb = math.sqrt(sigma_rg**2 + sigma_yb**2)
     mu_rgyb = math.sqrt(mu_rg**2 + mu_yb**2)
@@ -42,11 +42,20 @@ def naturalness(im):
 
 
 def contrast(im):
+    """return contrast"""
+    im = im.convert('L')
+    stat = ImageStat.Stat(im)
+    mean = stat.mean[0]
+    im_diff = ImageMath.eval("convert(a - b, 'L')", a=im, b=mean)
+    stat_diff = ImageStat.Stat(im_diff)
+    return stat_diff.sum2[0]/(stat_diff.count[0]-1)
+
+def sharpness(im):
     pass
 
 
 def main():
-    imgname = '1.jpg'
+    imgname = '5.jpg'
     im = Image.open(imgname)
-    print colorfulness(im)
+    print contrast(im)
 main()
